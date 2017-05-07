b6125f1bf8f2dc0c8eb2bc8725b6ee3886cb5ff8
Koh_Takada <koh.hatsushimo.tsuki@gmail.com>
2017-05-07 23:02:35 +0900

    YearSpecのitの文言を修正

--- spec/lib/year_spec.rb
+++ spec/lib/year_spec.rb
@@ -4,7 +4,7 @@ require 'year'
 describe 'Year' do
   describe 'leap_year?' do
     context '4の倍数の年の場合' do
-      it 'trueを返すこと' do
+      it 'trueを返す' do
         expect(Year.new(1996).leap_year?).to eq(true)
       end
     end
