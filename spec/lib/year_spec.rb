555ae09593b66f388207a829acd2f0fb83c1aa36
Koh_Takada <koh.hatsushimo.tsuki@gmail.com>
2017-05-08 20:16:42 +0900

    YearSpec 最初のテストケースを閏年でない年に変更

--- spec/lib/year_spec.rb
+++ spec/lib/year_spec.rb
@@ -3,9 +3,9 @@ require 'year'
 
 describe 'Year' do
   describe 'leap_year?' do
-    context '4の倍数の年の場合' do
-      it 'trueを返す' do
-        expect(Year.new(1996).leap_year?).to eq(true)
+    context '4の倍数でない場合' do
+      it 'falseを返す' do
+        expect(Year.new(1999).leap_year?).to eq(false)
       end
     end
   end
