3e9099fca0fb725e38ee5edd59c209f1d0b3c8c3
Koh_Takada <koh.hatsushimo.tsuki@gmail.com>
2017-05-08 22:43:13 +0900

    FizzBuzzSpecc 3の倍数でも5の倍数でもない場合の期待値を文字列に変更

--- spec/lib/fizz_buzz_spec.rb
+++ spec/lib/fizz_buzz_spec.rb
@@ -5,7 +5,7 @@ describe 'FizzBuzz' do
   describe 'of' do
     context '3の倍数でも5の倍数でもない場合' do
       it 'パラメータの値をそのまま返す' do
-        expect(FizzBuzz.of(1)).to eq(1)
+        expect(FizzBuzz.of(1)).to eq('1')
       end
     end
   end
