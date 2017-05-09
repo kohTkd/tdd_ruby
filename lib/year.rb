83fa5a790cf473009b279de0b657d866cd821733
Koh_Takada <koh.hatsushimo.tsuki@gmail.com>
2017-05-09 19:51:16 +0900

    Year#leap_year?()の実装例を作成

--- lib/year.rb
+++ lib/year.rb
@@ -3,5 +3,9 @@ class Year
     @year = year
   end
 
-  def leap_year?; end
+  def leap_year?
+    return false unless (@year % 4).zero?
+    return true unless (@year % 100).zero?
+    (@year % 400).zero?
+  end
 end
--- spec/lib/year_spec.rb
+++ spec/lib/year_spec.rb
@@ -8,5 +8,25 @@ describe 'Year' do
         expect(Year.new(1999).leap_year?).to eq(false)
       end
     end
+
+    context '4の倍数の場合' do
+      context '100の倍数でない場合' do
+        it 'trueを返す' do
+          expect(Year.new(2004).leap_year?).to eq(true)
+        end
+      end
+      context '100の倍数の場合' do
+        context '400の倍数でない場合' do
+          it 'falseを返す' do
+            expect(Year.new(2100).leap_year?).to eq(false)
+          end
+        end
+        context '400の倍数の場合' do
+          it 'trueを返す' do
+            expect(Year.new(2000).leap_year?).to eq(true)
+          end
+        end
+      end
+    end
   end
 end
