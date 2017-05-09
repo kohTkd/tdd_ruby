7dff45220b2ad050f24867b1ad6921d3d36a9018
Koh_Takada <koh.hatsushimo.tsuki@gmail.com>
2017-05-09 19:59:51 +0900

    FizzBuzz.of()の実装例を追加

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,8 @@
 module FizzBuzz
-  def self.of(_number); end
+  def self.of(number)
+    str = nil
+    str = 'Fizz' if (number % 3).zero?
+    str = "#{str}Buzz" if (number % 5).zero?
+    str || number.to_s
+  end
 end
--- spec/lib/fizz_buzz_spec.rb
+++ spec/lib/fizz_buzz_spec.rb
@@ -8,5 +8,20 @@ describe 'FizzBuzz' do
         expect(FizzBuzz.of(1)).to eq('1')
       end
     end
+    context '3の倍数の場合' do
+      it 'Fizzを返す' do
+        expect(FizzBuzz.of(3)).to eq('Fizz')
+      end
+    end
+    context '5の倍数の場合' do
+      it 'Buzzを返す' do
+        expect(FizzBuzz.of(5)).to eq('Buzz')
+      end
+    end
+    context '15の倍数の場合' do
+      it 'FizzBuzzを返す' do
+        expect(FizzBuzz.of(15)).to eq('FizzBuzz')
+      end
+    end
   end
 end
